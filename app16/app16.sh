8c4f1cf25e2b01dc8d934c025290872974d8204e
Developer <khrist14n@gmail.com>
2015-09-05 02:28:01 -0500

    app16

---
+++ app16/app16.sh
@@ -0,0 +1,5 @@
+echo 'App Tips de Programacion'
+echo 'construccion de Aplicacion'
+echo 'sencha -sdk /home/guest/sencha/framework/ext-6.0.0/  generate app --classic app16 ./app16'
+sencha -sdk /home/guest/sencha/framework/ext-6.0.0/  generate app --classic app16 ./app16
+echo '>>>>>> Fin de construccion de proyecto >>>>>'
